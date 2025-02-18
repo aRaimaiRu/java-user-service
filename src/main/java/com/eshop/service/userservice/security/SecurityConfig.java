408d667e111006c930f4b855e3b5a30947d26c4e
Kittipat <first1801@hotmail.com>
2025-02-18 17:04:52 +0700

    remove comment

--- src/main/java/com/eshop/service/userservice/security/SecurityConfig.java
+++ src/main/java/com/eshop/service/userservice/security/SecurityConfig.java
@@ -47,16 +47,4 @@ public class SecurityConfig {
                 .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
         return http.build();
     }
-
-//    @Bean
-//    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-//        http
-//                .authorizeHttpRequests(authorizeRequests ->
-//                        authorizeRequests
-//                                .requestMatchers("/public/**").permitAll()
-//                                .anyRequest().authenticated()
-//                )
-//                .oauth2ResourceServer((oauth2) -> oauth2.jwt(Customizer.withDefaults()));
-//        return http.build();
-//    }
 }
